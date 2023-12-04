04a17c8f9b3fac4035acd67ceef72f8a0e233b54
liuzhenwei <dui_zhang@163.com>
2023-12-04 21:43:35 +0800

    add sample service framework
    Signed-off-by: liuzhenwei <dui_zhang@163.com>

---
+++ cmd/serviceA/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"k8s.io/klog"
+
+	"kubernetes-workshop/pkg/handlers"
+)
+
+func main() {
+	h := handlers.KubernetesWorkshop{
+		ServiceTag: "serviceA",
+	}
+	r := gin.Default()
+	r.GET("/info", h.Info)
+	if err := r.Run(); err != nil {
+		klog.Fatal(err)
+	}
+}
---
+++ cmd/serviceB/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"k8s.io/klog"
+
+	"kubernetes-workshop/pkg/handlers"
+)
+
+func main() {
+	h := handlers.KubernetesWorkshop{
+		ServiceTag: "serviceB",
+	}
+	r := gin.Default()
+	r.GET("/info", h.Info)
+	if err := r.Run(); err != nil {
+		klog.Fatal(err)
+	}
+}
